23f5002eeb657ac7f7b355c316dbd60c27225654
Sam DePoule <samdepoule@gmail.com>
2025-03-03 16:28:45 -0800

    setup build env with github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,4 +16,26 @@ const config = {
 	}
 };
 
-export default config;
+// export default config;
+
+export default {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// Paths for static sites
+		adapter: adapter({
+			pages: 'docs',   // Specifies where to output the build
+			assets: 'docs',  // Specifies the folder for assets
+			fallback: 'index.html',  // Set to 'index.html' if you want a fallback
+		}),
+
+		// // Optional: If you want to adjust Vite-specific settings (e.g., for dev or build process)
+		// vite: {
+		// 	build: {
+		// 		outDir: 'docs', // Custom output folder for static files
+		// 	},
+		// },
+	},
+};
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	build: {
+		outDir: 'docs',
+		rollupOptions: {
+			input: 'src/app.html'
+		}
+	},
 	plugins: [sveltekit()]
 });
