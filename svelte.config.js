fb02e363f322d7039c628a4b015573c70073d3c6
Sam DePoule <samdepoule@gmail.com>
2025-03-03 18:13:22 -0800

    ssg build

--- svelte.config.js
+++ svelte.config.js
@@ -12,26 +12,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		// adapter: adapter()
 
-// export default config;
-
-export default {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
 		// Paths for static sites
 		adapter: adapter({
 			pages: 'docs',   // Specifies where to output the build
-			// assets: 'docs',  // Specifies the folder for assets
-			fallback: 'index.html',  // Set to 'index.html' if you want a fallback
+			assets: 'docs',  // Specifies the folder for assets
+			fallback: '404.html',  // Set to 'index.html' if you want a fallback
+			precompress: false,
+			strict: true
 		}),
 		paths: {
-			base: '/DijkstrasAlogirthm',  // The subdirectory where your app is hosted
+			// base: '/DijkstrasAlogirthm',  // The subdirectory where your app is hosted
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 
 
@@ -41,5 +34,7 @@ export default {
 		// 		outDir: 'docs', // Custom output folder for static files
 		// 	},
 		// },
-	},
+	}
 };
+
+export default config;
